ca91a2b83181bd869c5ddad47886e842a2f8828e
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-01-03 21:23:52 +0900

    :up: add sources

---
+++ src/api.js
@@ -0,0 +1,100 @@
+// api.js - This module is a shallow wrapper of paiza.io API.
+
+var
+_       = require('lodash'),
+debug   = require('debug')('paiza-io:api'),
+request = require('request');
+
+var
+config = require('./config');
+
+// === utility ===
+
+function create_api_parameter(parameter) {
+  return _.defaults(_.clone(parameter), config.default_parameter);
+}
+
+function wrap_request(method, base_url, path, parameter, callback) {
+  debug('request start');
+  debug('method = %s', method);
+  debug('base_url = %s', base_url);
+  debug('path = %s', path);
+  debug('parameter = %o', parameter);
+
+  var
+  options = {
+    method: method,
+    url: base_url + path,
+    json: true,
+  };
+  parameter = create_api_parameter(parameter);
+  if (method.toUpperCase() === 'POST') {
+    options.form = parameter;
+  } else {
+    options.qs = parameter;
+  }
+
+  request(options, function request_callback(error, response, body) {
+    if (error) return callback(error);
+
+    if (body.error) {
+      error = new Error('paiza.io error: ' + body.error);
+      error.paizaIoMessage = body.error;
+      return callback(error);
+    }
+
+    body.http_response = response; // extends body object for useful.
+    return callback(null, body);
+  });
+}
+
+// === exports ===
+
+// wrapper of runners/create
+function create(parameter, base_url /* this parameter exists for testing */, callback) {
+  if (!callback) {
+    callback = base_url;
+    base_url = '';
+  }
+
+  return wrap_request(
+    config.api.create.method,
+    base_url || config.base_url,
+    config.api.create.path,
+    parameter,
+    callback);
+}
+
+// wrapper of runners/get_status
+function get_status(parameter, base_url /* this parameter exists for testing */, callback) {
+  if (!callback) {
+    callback = base_url;
+    base_url = '';
+  }
+
+  return wrap_request(
+    config.api.get_status.method,
+    base_url || config.base_url,
+    config.api.get_status.path,
+    parameter,
+    callback);
+}
+
+// wrapper of runners/get_details
+function get_details(parameter, base_url /* this parameter exists for testing */, callback) {
+  if (!callback) {
+    callback = base_url;
+    base_url = '';
+  }
+
+  return wrap_request(
+    config.api.get_details.method,
+    base_url || config.base_url,
+    config.api.get_details.path,
+    parameter,
+    callback);
+}
+
+exports.create = create;
+exports.get_status = get_status;
+exports.get_details = get_details;
---
+++ src/paiza-io.js
@@ -0,0 +1,76 @@
+// paiza-io.js - this module is the wrapper of paiza.io API.
+
+var
+_     = require('lodash'),
+debug = require('debug')('paiza-io');
+
+var
+api = require('./api'),
+config = require('./config');
+
+// === exports ===
+
+function paiza_io(language, code, input, option, callback) {
+  // `parameter` is a optional parameter.
+  if (!callback) {
+    callback = option;
+    option = {};
+  }
+  option = _.defaults(_.clone(option), {
+    base_url: config.base_url,
+    retry_get_status_time: 1000,
+    max_get_status_loop: 10,
+  });
+
+  var
+  parameter = _.assign({
+    language: language,
+    source_code: code,
+    input: input,
+  }, option.parameter);
+
+  create(parameter, option, callback);
+}
+
+function create(parameter, option, callback) {
+  debug('create phase');
+  api.create(parameter, get_status(option, callback));
+}
+
+function get_status(option, callback) {
+  return function create_callback(error, body) {
+    if (error) return callback(error);
+    debug('response ==> %o', body);
+    debug('get_status phase');
+
+    (function loop(count, body) {
+      debug('get_status loop (%d)', count);
+      if (body.status === 'completed') {
+        return get_details(body.id, option, callback);
+      }
+      if (count >= option.max_get_status_loop) {
+        // TODO: change an error message to courteouseness.
+        return callback(new Error('get_status_loop overflow!'));
+      }
+
+      setTimeout(function get_status_loop_timeout() {
+        api.get_status({
+          id: body.id,
+        }, function (error, body) {
+          if (error) return callback(error);
+          debug('response ==> %o', body);
+          loop(count + 1, body);
+        });
+      }, option.retry_get_status_time);
+    })(0, body);
+  }
+}
+
+function get_details(id, option, callback) {
+  debug('get_details phase');
+  api.get_details({
+    id: id,
+  }, callback);
+}
+
+module.exports = paiza_io;
